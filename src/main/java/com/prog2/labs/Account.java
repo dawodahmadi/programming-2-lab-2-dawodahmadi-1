65225e4a272d60535389f3b676ca9c534518cc1b
dawodahmadi <ahmadi.dawod@gmail.com>
2023-02-18 17:40:52 -0500

    first commit

---
+++ src/main/java/com/prog2/labs/Account.java
@@ -0,0 +1,90 @@
+package com.prog2.labs;
+/**
+ * @author adinashby
+ *
+ */
+public class Account {
+	
+	/**
+	 * i) Define three instance variables called accountNumber, name and amount.
+	 */
+	
+	/**
+	 * ii) Define one static variable called numberOfAccounts.
+	 */
+	
+	/**
+	 * Write your test code below in the main (optional).
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * iii) A default constructor.
+	 */
+	
+	/**
+	 * iv) A constructor that takes all the parameters and initializes them 
+	 */
+	
+	public Account(int accountNumber, String name, double amount) {
+		
+	}
+	
+	/**
+	 * v) Getters and setters (accessors and mutators).
+	 */
+	
+	/**
+	 * vi) Adds the parameter value to the amount field.
+	 * 
+	 * @param amountToBeDeposited a positive number.
+	 *
+	 * @return result new value.
+	 */
+	public double deposit(double amountToBeDeposited) {
+		return 0.0;
+	}
+	
+	/**
+	 * vii) Subtracts the parameter value from the amount field.
+	 * 
+	 * @param amountToBeWithdrawn a positive number.
+	 *
+	 * @return result new value.
+	 */
+	public double withdraw(double amountToBeWithdrawn) {
+		return 0.0;
+	}
+	
+	/**
+	 * viii) Adds %2 interest to the amount field.
+	 *
+	 * @return result new value.
+	 */
+	public double calculateInterest() {
+		return 0.0;
+	}
+	
+	/**
+	 * ix) To show the values inside the object .
+	 *
+	 * @return result the string containing the values.
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * x) To compare if two instances of Account class are equal or not.
+	 *
+	 * @param anotherAccount another instance of the Account class.
+	 *
+	 * @return result the string containing the values.
+	 */
+	public boolean equals(Account anotherAccount) {
+		return false;
+	}
+}
---
+++ src/main/java/com/prog2/labs/LabTwo.java
@@ -0,0 +1,45 @@
+package com.prog2.labs;
+/**
+ * @author adinashby
+ *
+ */
+public class LabTwo {
+
+	/**
+	 * Write your test code below in the main (optional).
+	 *
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * Please refer to the README file for question(s) description
+	 */
+	
+	public double calcAmountOfShipping(double costOfPurchase) {
+		
+		
+		return 0.0;
+	}
+	
+	
+	public String findPrimeNumbersUptoInput(int input) {
+		
+		
+		return "";
+	}
+	
+	
+	public String generateShape(int numberOfRows) {
+		
+		
+		return "";
+	}
+	
+	/**
+	 * 4) Complete the Account class (see inside the class for instruction). 
+	 * 
+	 */
+
+}
